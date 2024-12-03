e3afddc205e91a89e16050032cc4ba8f39c3d1dd
Jbilac <79338162+jbilac1@users.noreply.github.com>
2024-12-03 10:55:20 +0100

    removed comments

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const translations = {
         
     },
     hr: {
-        pricelist: 'Cjenik pića'
+        pricelist: 'Cjenik Pića'
     }
 };
 
@@ -27,17 +27,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use(favicon(__dirname + '/favicon.ico'))
 
-
-
 app.get("/", setLanguage, (req, res)=>{
     
     res.render('pages/home', {pricelist, languages: Object.keys(translations)});
 })
-// Language change route
+
 app.get('/change-language', (req, res) => {
     const lang = req.query.lang;
-    
-    // Validate language
     if (Object.keys(translations).includes(lang)) {
         res.cookie('language', lang, { 
             maxAge: 365 * 24 * 60 * 60 * 1000, 
@@ -45,10 +41,8 @@ app.get('/change-language', (req, res) => {
         });
     }
 
-    // Redirect back to previous page or home
     res.redirect(req.get('referer') || '/');
 });
-
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}"`);
